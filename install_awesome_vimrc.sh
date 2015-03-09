1f08d0d169193847194ec89a9a05900cfe6ba0a2
seba-1511 <seba-1511@hotmail.com>
2015-03-09 12:40:40 -0700

    added installation of vim-autoformat.

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -4,6 +4,8 @@ sudo apt-get install python-autopep8
 
 echo 'set runtimepath+=~/.vim_runtime
 
+wget https://raw.githubusercontent.com/Chiel92/vim-autoformat/master/plugin/autoformat.vim sources_non_forked/autoformat.vim
+
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
